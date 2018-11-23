448168c77931d3146014156f27011ca0e62fc047
hakim_helmy <hakim.helmy@gmail.com>
2018-11-22 20:38:31 -0500

    ggg

--- WebApplication8/Startup.cs
+++ WebApplication8/Startup.cs
@@ -13,7 +13,7 @@ namespace WebApplication8
         {
             Configuration = configuration;
         }
-        //Start1
+        //Start3
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
